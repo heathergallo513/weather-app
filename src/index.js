d11fa321db1a50cf33163be12fc1bcce14f65a77
heathergallo513 <128610780+heathergallo513@users.noreply.github.com>
2023-05-12 22:37:30 -0400

    began work on API integration for search

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function displayTemperature(response) {
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+}
+
+let apiKey = "743bee57fddbfaf52447193a87d5dd25";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
